4c15908e34ce064295fb2ac58c681a6ec7035aaa
Extra <47879832+ma1833577561@users.noreply.github.com>
2021-06-04 13:22:39 +0800

    Update base.js

--- Axios/Axios and  Interface packaging/api/base.js
+++ Axios/Axios and  Interface packaging/api/base.js
@@ -1,24 +1,24 @@
 // 域名文件
 const base = { 
-	 m3: 'https://mo03.lhhc.cn',
+	 m3: 'https://qmo03.qyfw.com',
 	 test: 'https://192.168.31.35:7012', // 【本地调试】
 	 testA: 'http://192.168.31.35:7082', // 【本地调试】
 	 test1: 'https://192.168.31.135:7504', // 【本地调试-拨打电话】
-	 hr_org: 'https://c22.lhhc.cn', // 【人事管理】
-	 cpc_customer: 'https://e10106.lhhc.cn', // 【客户管理】
-	 cpc_provider: 'https://e10106.lhhc.cn', // 【供应商管理】
-	 sv_workflow: 'https://b501.lhhc.cn', // 【工作流管理】
-	 psi_po: 'https://e10182.lhhc.cn', // 【订单管理】
-	 psi_storage: 'https://e10183.lhhc.cn', // 【库存管理】
-	 pm_projectmanage: 'https://e56.lhhc.cn', // 【项目管理】
-	 fa_financecompany: 'https://e41.lhhc.cn', // 【执照管理】
-	 fa_finance: 'https://e42.lhhc.cn', // 【财务管理】
-	 fa_expense: 'https://e10112.lhhc.cn', // 【报销管理】
-	 fa_contract: 'https://e10111.lhhc.cn', // 【合同管理】
-	 sv_phone: 'https://b508.lhhc.cn', // 【通话记录】
-	 fa_mosmetic: 'https://e52.lhhc.cn',// 【商城管理】
-	 sv_document: 'https://mo03.lhhc.cn', // 【附件管理】
-	 pattern_e83: 'https://e83.lhhc.cn',
-	 sv_message: 'https://b501.lhhc.cn'
+	 hr_org: 'https://qc22.qyfw.cn', // 【人事管理】
+	 cpc_customer: 'https://qe10106.qyfw.com', // 【客户管理】
+	 cpc_provider: 'https://qe10106.qyfw.com', // 【供应商管理】
+	 sv_workflow: 'https://qb501.qyfw.com', // 【工作流管理】
+	 psi_po: 'https://qe10182.qyfw.com', // 【订单管理】
+	 psi_storage: 'https://qe10183.qyfw.com', // 【库存管理】
+	 pm_projectmanage: 'https://qe56.qyfw.com', // 【项目管理】
+	 fa_financecompany: 'https://qe41.qyfw.com', // 【执照管理】
+	 fa_finance: 'https://qe42.qyfw.com', // 【财务管理】
+	 fa_expense: 'https://qe10112.qyfw.com', // 【报销管理】
+	 fa_contract: 'https://qe10111.qyfw.com', // 【合同管理】
+	 sv_phone: 'https://qb508.qyfw.com', // 【通话记录】
+	 fa_mosmetic: 'https://qe52.qyfw.com',// 【商城管理】
+	 sv_document: 'https://qmo03.qyfw.com', // 【附件管理】
+	 pattern_e83: 'https://qe83.qyfw.com',
+	 sv_message: 'https://qb501.qyfw.com'
 }
 export default base
